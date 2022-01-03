1fd26f9494686e11cda691602e6a9ed1c5a70d09
Bùi Tuấn Tâm <tambt@tinhvan.com>
2022-01-03 17:34:22 +0700

    Add all my files

---
+++ UserService/DataContext/UserServiceContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.DataContext
+{
+    public class UserServiceContext : DbContext
+    {
+        public UserServiceContext(DbContextOptions<UserServiceContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<UserService.Entities.User> User { get; set; }
+    }
+}
